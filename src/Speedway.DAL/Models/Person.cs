86d96911fa798af3dfe252bae832f79ddd3ec62b
kingkong <ckmkingkong@gmail.com>
2017-09-30 16:02:11 +0200

    First Commit

---
+++ src/Speedway.DAL/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Speedway.DAL.Models
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
