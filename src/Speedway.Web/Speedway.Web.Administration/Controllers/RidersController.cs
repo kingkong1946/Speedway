d876a43ee9536fa45f6fcf27ab71101ebf5b6de8
kingkong1946 <ckmkingkong@gmail.com>
2017-11-29 17:48:04 +0100

    Create Web Administrator application

---
+++ src/Speedway.Web/Speedway.Web.Administration/Controllers/RidersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Speedway.Web.Administration.Controllers
+{
+    public class RidersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
