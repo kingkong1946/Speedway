02f37915347b524d6b1314b07a68557fa3695c0a
kingkong <ckmkingkong@gmail.com>
2018-02-08 19:59:48 +0100

    fix ng routing module

--- src/Speedway.Web/Speedway.Web.Administration/Speedway.Web.Administration.Client/speedway-web-administration-client/src/app/app-routing.module.ts
+++ src/Speedway.Web/Speedway.Web.Administration/Speedway.Web.Administration.Client/speedway-web-administration-client/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 
-import {PersonListComponent} from "./person-list/person-list.component";
-
 const routes: Routes = [
   // {path: '', redirectTo: '/persons', pathMatch: 'full'},
   // {path: 'persons', component: PersonListComponent}
