5f2b63b7b178a3d4342001d12fead6e78d6d40e9
kingkong <ckmkingkong@gmail.com>
2018-02-26 18:12:26 +0100

    Added CountryController and removed client project

---
+++ src/Speedway.Web/Speedway.Web.Administration/Speedway.Web.Administration.Server/Controllers/CountryController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Speedway.DAL.Models;
+
+namespace Speedway.Web.Administration.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountryController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Country> Index()
+        {
+            var persons = new List<Country>
+            {
+                new Country {Id = 0, Name = "Poland"},
+                new Country {Id = 1, Name = "Denmark"},
+                new Country {Id = 2, Name = "Sweden"},
+                new Country {Id = 3, Name = "Russia"},
+                new Country {Id = 4, Name = "Great Britain"},
+                new Country {Id = 5, Name = "Australia"}
+            };
+
+            return persons;
+        }
+    }
+}
--- src/Speedway.Web/Speedway.Web.Administration/Speedway.Web.Administration.Server/Startup.cs
+++ src/Speedway.Web/Speedway.Web.Administration/Speedway.Web.Administration.Server/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Speedway.DAL;
 
 namespace Speedway.Web.Administration.Server
 {
